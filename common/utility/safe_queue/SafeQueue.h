54711cd02ae7823fbb613d30b397310a5f74d22c
SiLViU <silviuptc@gmail.com>
2026-02-07 01:03:58 -1200

    Added namespace to SafeQueue

--- common/utility/safe_queue/SafeQueue.h
+++ common/utility/safe_queue/SafeQueue.h
@@ -6,56 +6,58 @@
 #include <condition_variable>
 #include <optional>
 
-template<typename T>
-class SafeQueue
+namespace sLink::utility
 {
-public:
-	SafeQueue() = default;
-
-	SafeQueue(const SafeQueue& other) = delete;
-
-	SafeQueue& operator=(const SafeQueue& other) = delete;
-
-	void push(const T& val)
+	template<typename T>
+	class SafeQueue
 	{
+	public:
+		SafeQueue() = default;
+
+		SafeQueue(const SafeQueue& other) = delete;
+
+		SafeQueue& operator=(const SafeQueue& other) = delete;
+
+		void push(const T& val)
+		{
+			{
+				std::lock_guard lock(m_Mutex);
+
+				m_Queue.push(val);
+			}
+			m_CondVar.notify_one();
+		}
+
+		void push(T&& val)
+		{
+			{
+				std::lock_guard lock(m_Mutex);
+
+				m_Queue.push(std::move(val));
+			}
+			m_CondVar.notify_one();
+		}
+
+		std::optional<T> tryPop()
 		{
 			std::lock_guard lock(m_Mutex);
 
-			m_Queue.push(val);
+			if (m_Queue.empty())
+				return std::nullopt;
+
+			auto value = std::move(m_Queue.front());
+
+			m_Queue.pop();
+
+			return value;
 		}
-		m_CondVar.notify_one();
-	}
 
-	void push(T&& val)
-	{
-		{
-			std::lock_guard lock(m_Mutex);
+	private:
+		std::queue<T> m_Queue;
 
-			m_Queue.push(std::move(val));
-		}
-		m_CondVar.notify_one();
-	}
+		std::mutex m_Mutex;
 
-	std::optional<T> tryPop()
-	{
-		std::lock_guard lock(m_Mutex);
-
-		if (m_Queue.empty())
-			return std::nullopt;
-
-		auto value = std::move(m_Queue.front());
-
-		m_Queue.pop();
-
-		return value;
-	}
-
-private:
-	std::queue<T> m_Queue;
-
-	std::mutex m_Mutex;
-
-	std::condition_variable m_CondVar;
-};
-
+		std::condition_variable m_CondVar;
+	};
+}
 #endif
