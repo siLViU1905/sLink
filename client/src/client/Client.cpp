b7bd683cbd9dd46d3da3c6df51fc587100ef3af8
SiLViU <silviuptc@gmail.com>
2026-02-07 02:13:10 -1200

    Implemented base Client structure

---
+++ client/src/client/Client.cpp
@@ -0,0 +1,94 @@
+#include "Client.h"
+
+namespace sLink::client
+{
+	Client::Client(asio::io_context& ctx) :m_IOContext(ctx), m_Socket(ctx), m_IsWriting(false)
+	{
+	}
+
+	void Client::connect(std::string_view host, std::string_view port)
+	{
+		asio::ip::tcp::resolver resolver(m_IOContext);
+
+		auto endpoints = resolver.resolve(host, port);
+
+		onConnect(endpoints);
+	}
+
+	void Client::send(std::string msg)
+	{
+		m_Outbox.push(std::move(msg));
+
+		asio::post(m_IOContext, [this]()
+			{
+				if (!m_IsWriting)
+					onWrite();
+			});
+	}
+
+	bool Client::isConnected() const
+	{
+		return m_Socket.is_open();
+	}
+
+	void Client::onConnect(asio::ip::tcp::resolver::results_type endpoints)
+	{
+		asio::async_connect(m_Socket, endpoints,
+			[this](std::error_code ec, asio::ip::tcp::endpoint)
+			{
+				if (!ec)
+				{
+					onRead();
+				}
+			});
+	}
+
+	void Client::onWrite()
+	{
+		auto msg = m_Outbox.tryPop();
+
+		if (!msg)
+		{
+			m_IsWriting = false;
+			return;
+		}
+
+		m_IsWriting = true;
+
+		auto to_send = *msg + "\n";
+
+		asio::async_write(m_Socket, asio::buffer(to_send),
+			[this](std::error_code ec, size_t)
+			{
+				if (!ec)
+					onWrite();
+				else
+				{
+					m_IsWriting = false;
+					m_Socket.close();
+				}
+			});
+	}
+
+	void Client::onRead()
+	{
+		asio::async_read_until(m_Socket, m_ReadBuffer, '\n',
+			[this](std::error_code ec, size_t length)
+			{
+				if (!ec)
+				{
+					std::istream is(&m_ReadBuffer);
+
+					std::string line;
+
+					std::getline(is, line);
+
+					m_Inbox.push(std::move(line));
+
+					onRead();
+				}
+				else
+					m_Socket.close();
+			});
+	}
+}
---
+++ client/src/client/Client.h
@@ -0,0 +1,40 @@
+#ifndef SLINK_CLIENT_H
+#define SLINK_CLIENT_H
+
+#include <utility/safe_queue/SafeQueue.h>
+#include <asio.hpp>
+
+namespace sLink::client
+{
+	class Client
+	{
+	public:
+		Client(asio::io_context& ctx);
+
+		void connect(std::string_view host, std::string_view port);
+
+		void send(std::string msg);
+
+		bool isConnected() const;
+	private:
+		void onConnect(asio::ip::tcp::resolver::results_type endpoints);
+
+		void onWrite();
+
+		void onRead();
+
+		asio::io_context& m_IOContext;
+
+		asio::ip::tcp::socket m_Socket;
+
+		SafeQueue<std::string> m_Inbox;
+
+		SafeQueue<std::string> m_Outbox;
+
+		asio::streambuf m_ReadBuffer;
+
+		bool m_IsWriting;
+	};
+}
+
+#endif
