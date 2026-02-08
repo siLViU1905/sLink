3cd878ea6da6d520c4b023d80b7c028fd101dc20
SiLViU <silviuptc@gmail.com>
2026-02-08 20:52:54 +0200

    Fancier ChatWindow

--- common/ui/components/chat_window/UIChatWindow.cpp
+++ common/ui/components/chat_window/UIChatWindow.cpp
@@ -4,18 +4,17 @@
 
 namespace sLink::ui::component
 {
-	UIChatWindow::UIChatWindow():m_InputContent(255, 0)
-	{
-	}
+    UIChatWindow::UIChatWindow() : m_InputContent(255, 0)
+    {
+    }
 
     void UIChatWindow::render()
     {
         ImGui::SetNextWindowPos(ImVec2(0, 0));
-
         ImGui::SetNextWindowSize(ImGui::GetIO().DisplaySize);
 
         ImGui::PushStyleVar(ImGuiStyleVar_Alpha, 1.0f);
-        
+
         constexpr ImGuiWindowFlags window_flags = ImGuiWindowFlags_NoTitleBar |
             ImGuiWindowFlags_NoResize |
             ImGuiWindowFlags_NoMove |
@@ -24,50 +23,84 @@ namespace sLink::ui::component
 
         ImGui::Begin("Chat Window", nullptr, window_flags);
 
-        ImGui::BeginChild("ScrollingRegion", ImVec2(0, -ImGui::GetFrameHeightWithSpacing()), false);
+        ImGui::SetWindowFontScale(s_FontScale);
+
+        float scaled_line_height = ImGui::GetTextLineHeightWithSpacing() * s_FontScale;
+        float footer_height = s_ButtonHeight + ImGui::GetStyle().ItemSpacing.y * 2;
+
+        ImGui::BeginChild("ScrollingRegion", ImVec2(0, -footer_height), false);
+
         for (const auto& message : m_Messages)
-            ImGui::Text("[%lld] %s: %s", message.getTimestamp(), message.getSenderName().data(), message.getContent().data());
+        {
+            ImGui::BeginGroup();
+
+            ImGui::TextColored(s_ColorTimestamp, "[%s]", message.getTimestamp().toString().c_str());
+
+            ImGui::SameLine();
+            ImGui::TextColored(s_ColorUsername, "%s:", message.getSenderName().data());
+
+            ImGui::SameLine();
+            ImGui::TextColored(s_ColorContent, "%s", message.getContent().data());
+
+            ImGui::EndGroup();
+
+            ImGui::Dummy(ImVec2(0.0f, s_MessageSpacing));
+        }
+        if (ImGui::GetScrollY() >= ImGui::GetScrollMaxY())
+            ImGui::SetScrollHereY(1.0f);
 
         ImGui::EndChild();
 
         ImGui::Separator();
 
-        constexpr float button_width = 60.f;
-
         float spacing = ImGui::GetStyle().ItemSpacing.x;
 
-        ImGui::PushItemWidth(ImGui::GetContentRegionAvail().x - button_width - spacing);
+        float available_width = ImGui::GetContentRegionAvail().x;
+
+        ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, ImVec2(ImGui::GetStyle().FramePadding.x, s_InputPaddingY));
+
+        ImGui::PushItemWidth(available_width - s_ButtonWidth - spacing);
 
         bool input_submitted = ImGui::InputText("##input", m_InputContent.data(), 256, ImGuiInputTextFlags_EnterReturnsTrue);
 
         ImGui::PopItemWidth();
+        ImGui::PopStyleVar();
 
         ImGui::SameLine();
 
-        if (ImGui::Button("Send", ImVec2(button_width, 0)) || input_submitted)
+        ImGui::PushStyleVar(ImGuiStyleVar_FrameRounding, s_ButtonRounding);
+        ImGui::PushStyleColor(ImGuiCol_Button, s_ColorButton);
+        ImGui::PushStyleColor(ImGuiCol_ButtonHovered, s_ColorButtonHovered);
+        ImGui::PushStyleColor(ImGuiCol_ButtonActive, s_ColorButtonActive);
+
+        if (ImGui::Button("Send", ImVec2(s_ButtonWidth, s_ButtonHeight)) || input_submitted)
+        {
             if (m_InputContent[0] != '\0')
             {
                 if (m_OnSendCallback)
                 {
                     auto clean_content = m_InputContent.substr(0, m_InputContent.find('\0'));
-
                     m_OnSendCallback(clean_content);
                 }
-
                 m_InputContent[0] = '\0';
+                ImGui::SetKeyboardFocusHere(-1);
             }
+        }
+
+        ImGui::PopStyleColor(3);
+        ImGui::PopStyleVar();
 
         ImGui::End();
-        ImGui::PopStyleVar(); 
+        ImGui::PopStyleVar();
     }
 
-	void UIChatWindow::addMessage(const message::Message& message)
-	{
-		m_Messages.push_back(message);
-	}
+    void UIChatWindow::addMessage(const message::Message &message)
+    {
+        m_Messages.push_back(message);
+    }
 
-	void UIChatWindow::setOnMessageSend(OnSendCallback&& callback)
-	{
+    void UIChatWindow::setOnMessageSend(OnSendCallback &&callback)
+    {
         m_OnSendCallback = std::move(callback);
-	}
+    }
 }
--- common/ui/components/chat_window/UIChatWindow.h
+++ common/ui/components/chat_window/UIChatWindow.h
@@ -5,11 +5,29 @@
 #include <message/Message.h>
 #include <vector>
 #include <functional>
+#include <imgui.h>
 
 namespace sLink::ui::component
 {
     class UIChatWindow: public UIComponent
     {
+    private:
+        static constexpr float s_FontScale = 2.15f;
+        static constexpr float s_MessageSpacing = 6.f;
+
+        static constexpr float s_InputPaddingY = 10.f;
+        static constexpr float s_ButtonWidth = 80.f;
+        static constexpr float s_ButtonHeight = 40.f;
+        static constexpr float s_ButtonRounding = 8.0;
+
+        static constexpr ImVec4 s_ColorTimestamp = { 0.6f, 0.6f, 0.6f, 1.0f };
+        static constexpr ImVec4 s_ColorUsername  = { 0.2f, 0.7f, 0.9f, 1.0f };
+        static constexpr ImVec4 s_ColorContent   = { 1.0f, 1.0f, 1.0f, 1.0f };
+
+        static constexpr ImVec4 s_ColorButton        = { 0.15f, 0.45f, 0.85f, 1.f };
+        static constexpr ImVec4 s_ColorButtonHovered = { 0.2f, 0.55f, 0.95f, 1.f };
+        static constexpr ImVec4 s_ColorButtonActive  = { 0.1f, 0.35f, 0.75f, 1.f };
+
     public:
         using OnSendCallback = std::move_only_function<void(std::string_view)>;
 
