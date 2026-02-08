054776af64734b94e68f5c424c50b9ccd3ba8b95
SiLViU <silviuptc@gmail.com>
2026-02-08 13:21:07 +0200

    Improved UI layers architecture

--- common/ui/layers/UILayer.h
+++ common/ui/layers/UILayer.h
@@ -1,20 +1,14 @@
 #ifndef SLINK_UI_LAYER_H
 #define SLINK_UI_LAYER_H
 
-#include <vector>
-#include <memory>
-#include <components/UIComponent.h>
-
 namespace sLink::ui::layer
 {
     class UILayer
     {
     public:
-        void render();
-
-        void addComponent(std::unique_ptr<component::UIComponent>&& component);
-    private:
-        std::vector<std::unique_ptr<component::UIComponent>> m_Components;
+        virtual void render() = 0;
+    
+        virtual ~UILayer() = default;
     };
 }
 #endif
