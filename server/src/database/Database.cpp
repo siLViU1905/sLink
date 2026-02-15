986ec0a0ad96cd98b2e46da583e3b3c17411af86
SiLViU <silviuptc@gmail.com>
2026-02-15 10:57:51 +0200

    Added user and message table creation, user insertion logic, and updated SQLite mappings.

--- server/src/database/Database.cpp
+++ server/src/database/Database.cpp
@@ -1,5 +1,53 @@
 #include "Database.h"
 
+#include <format>
+
 namespace sLink::server::db
 {
+    Database::Database() : m_DatabaseHandle(nullptr)
+    {
+    }
+
+    std::expected<std::string, std::string> Database::start()
+    {
+        if (sqlite3_open(s_DatabaseName.data(), &m_DatabaseHandle) != SQLITE_OK)
+            return std::unexpected(std::format("Failed to start database {}", s_DatabaseName));
+
+        char *err;
+
+        if (sqlite3_exec(m_DatabaseHandle, s_CreateUsersTableQuery.data(), nullptr, nullptr, &err) != SQLITE_OK)
+            return std::unexpected(std::format("Failed to create users table. Error: {}", err));
+
+        if (sqlite3_exec(m_DatabaseHandle, s_CreateMessagesTableQuery.data(), nullptr, nullptr, &err) != SQLITE_OK)
+            return std::unexpected(std::format("Failed to create messages table. Error: {}", err));
+
+        return {"Database successfully started"};
+    }
+
+    std::expected<std::string, std::string> Database::addUser(std::string_view username)
+    {
+        sqlite3_stmt *stmt;
+
+        if (sqlite3_prepare_v2(m_DatabaseHandle, s_InsertUserQuery.data(), -1, &stmt, nullptr) != SQLITE_OK)
+            return std::unexpected(std::format("Failed to prepare insert user query"));
+
+        sqlite3_bind_text(stmt, 1, username.data(), -1, SQLITE_TRANSIENT);
+
+        if (sqlite3_step(stmt) != SQLITE_DONE)
+        {
+            sqlite3_finalize(stmt);
+
+            return std::unexpected(std::format("Failed to add user '{}'", username));
+        }
+
+        sqlite3_finalize(stmt);
+
+        return {std::format("User '{}' successfully added", username)};
+    }
+
+    Database::~Database()
+    {
+        if (m_DatabaseHandle)
+            sqlite3_close(m_DatabaseHandle);
+    }
 }
--- server/src/database/Database.h
+++ server/src/database/Database.h
@@ -2,6 +2,11 @@
 #define SLINK_DATABASE_H
 #include <string_view>
 
+#include <sqlite3.h>
+#include <memory>
+#include <string>
+#include <expected>
+
 namespace sLink::server::db
 {
     class Database
@@ -9,10 +14,33 @@ namespace sLink::server::db
     private:
         static constexpr std::string_view s_DatabaseName = "sLink_storage.db";
 
-    public:
+        static constexpr std::string_view s_CreateUsersTableQuery =
+            "CREATE TABLE IF NOT EXISTS users ("
+            "id INTEGER PRIMARY KEY AUTOINCREMENT, "
+            "username VARCHAR(25) NOT NULL"
+            ");";
 
+        static constexpr std::string_view s_CreateMessagesTableQuery =
+            "CREATE TABLE IF NOT EXISTS messages ("
+            "id INTEGER PRIMARY KEY AUTOINCREMENT, "
+            "content VARCHAR(255), "
+            "user_id INTEGER, "
+            "FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE"
+            ");";
+
+        static constexpr std::string_view s_InsertUserQuery = "INSERT INTO users (username) VALUES (?);";
+
+    public:
+        Database();
+
+        std::expected<std::string, std::string> start();
+
+        std::expected<std::string, std::string> addUser(std::string_view username);
+
+        ~Database();
 
     private:
+        sqlite3* m_DatabaseHandle;
     };
 }
 
